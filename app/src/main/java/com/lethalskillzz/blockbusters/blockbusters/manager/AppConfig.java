5a043aa6772c8d15de4973c426e639a7b4cb1d23
Ibrahim Abdulkadir <lethalskillzz@gmail.com>
2017-04-15 09:12:52 +0100

    revert changes

---
+++ app/src/main/java/com/lethalskillzz/blockbusters/blockbusters/manager/AppConfig.java
@@ -0,0 +1,39 @@
+package com.lethalskillzz.blockbusters.blockbusters.manager;
+
+/**
+ * Created by ibrahimabdulkadir on 12/04/2017.
+ */
+
+public class AppConfig {
+
+    // TMDB API url
+    public static String BASE_API_URL = "http://api.themoviedb.org/3/";
+
+
+    // Get popular movies
+    // GET /movie/popular
+    public static final String GET_POPULAR = "movie/popular";
+
+
+    // Get top rated movies
+    // GET /movie/top_rated
+    public static final String GET_TOP_RATED = "movie/top_rated";
+
+
+    // TMDB image url
+    public static final String BASE_IMG_URL = "http://image.tmdb.org/t/p/";
+
+    public static final String DISC_IMAGE_SIZE = "w185/";
+    public static final String DETAIL_IMAGE_SIZE = "w500/";
+
+
+    // TMDB API Key
+    public static final String API_KEY = "<YOUR API KEY>";
+
+
+    public static final String ORDER_TYPE_KEY = "orderTypeKey";
+    public static final String RESULT_KEY = "resultTempKey";
+
+    public static final String CLICK_GRID = "clickGrid";
+}
+
